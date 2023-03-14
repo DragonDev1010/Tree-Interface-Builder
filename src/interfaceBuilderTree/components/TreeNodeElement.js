dfa9cb8ba27677478e762148c8514ebd2eb5acee
DragonDev1010 <provic0099@gmail.com>
2023-03-14 12:41:36 -0400

    add tree-node-element-control-panel component

--- src/interfaceBuilderTree/components/TreeNodeElement.js
+++ src/interfaceBuilderTree/components/TreeNodeElement.js
@@ -1,60 +1,42 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { moveUp, moveDown, moveLeft, moveRight, createChild, deleteChild } from '../../redux/treeSlice'
-import { GoChevronDown, GoChevronLeft, GoChevronRight, GoChevronUp, GoPlus, GoTrashcan } from 'react-icons/go'
+import React, {useState} from 'react'
+import TreeNodeElementControlPanel from './TreeNodeElementControlPanel'
 
 export default function TreeNodeElement(props) {
-  const dispatch = useDispatch()
   const styles = {
+    container: {
+      borderRadius: 10,
+      marginBottom: 10,
+      display: 'flex',
+      alignItems: 'center',
+      gap: 10
+    },
     buttonStyle: {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center'
+    },
+    controlPanelContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-around'
     }
   }
+
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handleHover = () => { setIsHovered(true) }
+
+  const handleMouseLeave = () => { setIsHovered(false) }
+
   return (
-    <div
-      style={{
-        width: 200,
-        border: '1px solid blue',
-        padding: '5px 30px',
-        borderRadius: 10,
-        marginBottom: 10,
-      }}
+    <div 
+      style={styles.container} 
+      onMouseEnter = {handleHover}
+      onMouseLeave = {handleMouseLeave}
     >
-      <div
-        style={{
-          textAlign: 'center',
-          padding: 5
-        }}
-      >
+      <TreeNodeElementControlPanel isHovered = {isHovered} node={props.node}/>
+      <div style={{ textAlign: 'center', padding: 5, border: '1px solid #E6CCB2', cursor: 'pointer'}} >
         <span>ID : {props.node.id} : {props.node.value}</span>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-around'
-        }}
-      >
-        <button onClick={() => dispatch(moveUp(props.node.id))} style={styles.buttonStyle}>
-          <GoChevronUp />
-        </button>
-        <button onClick={() => dispatch(moveDown(props.node.id))} style={styles.buttonStyle}>
-          <GoChevronDown />
-        </button>
-        <button onClick={() => dispatch(moveLeft(props.node))} style={styles.buttonStyle}>
-          <GoChevronLeft />
-        </button>
-        <button onClick={() => dispatch(moveRight(props.node))} style={styles.buttonStyle}>
-          <GoChevronRight />
-        </button>
-        <button onClick={() => dispatch(createChild(props.node.id))} style={styles.buttonStyle}>
-          <GoPlus />
-        </button>
-        <button onClick={() => dispatch(deleteChild(props.node.id))} style={styles.buttonStyle}>
-          <GoTrashcan />
-        </button>
       </div>
     </div >
   )
